cec0750c50c8a1aa6da148572ea42133201e7444
rayrayaray <rayasya.dziqi@gmail.com>
2025-03-09 16:54:38 +0700

    feat: redirect to /maintenance

---
+++ src/app/maintenance/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import Particles from "@/app/components/ui/particles";
+
+export default function Maintenance() {
+  return (
+    <div className="relative w-full h-screen">
+      <Particles
+        particleColors={["#ffffff", "#ffffff"]}
+        particleCount={250}
+        particleSpread={5}
+        speed={0.1}
+        particleBaseSize={100}
+        moveParticlesOnHover={true}
+        alphaParticles={false}
+        disableRotation={false}
+      />
+      <div className="absolute top-0 flex justify-center items-center pointer-events-none h-screen w-full">
+        <h1 className="uppercase text-7xl">Under Development</h1>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,11 @@
 "use client";
-import Particles from "./components/ui/particles";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="relative w-full h-screen">
-      <Particles
-        particleColors={["#ffffff", "#ffffff"]}
-        particleCount={250}
-        particleSpread={5}
-        speed={0.1}
-        particleBaseSize={100}
-        moveParticlesOnHover={true}
-        alphaParticles={false}
-        disableRotation={false}
-      />
-      <div className="absolute top-0 flex justify-center items-center pointer-events-none h-screen w-full">
-        <h1 className="uppercase text-7xl">Under Development</h1>
-      </div>
-    </div>
-  );
+  const router = useRouter();
+  useEffect(() => {
+    router.replace("/maintenance");
+  });
+  return null;
 }

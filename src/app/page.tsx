1837ad49727d142f1a877830191bd8f7dac8782a
rayrayaray <rayasya.dziqi@gmail.com>
2025-03-09 15:48:58 +0700

    feature: change to under development page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,22 @@
 "use client";
-
-import { useState, useEffect, useRef } from "react";
-import gsap from "gsap";
-import SplashScreen from "./components/sections/splash_screen";
-import Hero from "./components/sections/hero";
+import Particles from "./components/ui/particles";
 
 export default function Home() {
-  const [showHero, setShowHero] = useState(false);
-  const revealRef = useRef(null);
-  const heroRef = useRef(null);
-
-  useEffect(() => {
-    if (showHero) {
-      gsap.fromTo(
-        heroRef.current,
-        { opacity: 0 },
-        { opacity: 1, duration: 0.5, ease: "power3.inOut" }
-      );
-    }
-  }, [showHero]);
-
-  const handleSplashComplete = () => {
-    gsap.to(revealRef.current, {
-      clipPath: "circle(150% at center)",
-      duration: 1,
-      ease: "power3.inOut",
-      onComplete: () => {
-        setShowHero(true);
-      },
-    });
-  };
-
   return (
-    <div className="relative w-screen h-screen bg-[#161616] overflow-hidden">
-      {!showHero && <SplashScreen onComplete={handleSplashComplete} />}
-
-      <div ref={heroRef} className="absolute inset-0 opacity-0">
-        <Hero />
+    <div className="relative w-full h-screen">
+      <Particles
+        particleColors={["#ffffff", "#ffffff"]}
+        particleCount={250}
+        particleSpread={5}
+        speed={0.1}
+        particleBaseSize={100}
+        moveParticlesOnHover={true}
+        alphaParticles={false}
+        disableRotation={false}
+      />
+      <div className="absolute top-0 flex justify-center items-center pointer-events-none h-screen w-full">
+        <h1 className="uppercase text-7xl">Under Development</h1>
       </div>
-
-      {!showHero && (
-        <div
-          ref={revealRef}
-          className="absolute inset-0 flex items-center justify-center bg-[#161616]"
-          style={{ clipPath: "circle(0% at center)" }}
-        />
-      )}
     </div>
   );
 }
